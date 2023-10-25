b45e50265aeaa5349247578e5d3dc800ff56f60e
Hoa Chau <hoacm@vng.com.vn>
2023-10-25 19:17:18 +0700

    move load model to app

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, jsonify, request, render_template, redirect
 from flask_cors import CORS
 import os
-from detection.detect import run_yolo_inference
+from detection.detect import run_yolo_inference, load_yolo_model
 from detection.process_bounding_boxes import process_bounding_boxes
-from ocr.vietocr_detect import perform_ocr_and_combine_text_for_sorted_images
+from ocr.vietocr_detect import perform_ocr_and_combine_text_for_sorted_images, load_vietocr_model
 
 app = Flask(__name__)
+
+model_folder = os.path.join('detection', 'model')
+# Define the path to the 'model.pt' file within the 'model' folder
+model_file_path = os.path.join(model_folder, 'model.pt')
+yolo_model = load_yolo_model(model_file_path)
+
+detector = load_vietocr_model()
 
 # Initialize CORS extension with specific options
 cors = CORS(app, resources={r"/api/*": {"origins": "https://localhost:8080"}})
@@ -16,24 +23,21 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 
+
 @app.route("/api/test", methods=["GET"])
 def test():
     # List all files in the image directory
     image_files = os.listdir(UPLOAD_FOLDER)
-    image_path = os.path.join(UPLOAD_FOLDER, image_files[2])
-
-    model_folder = os.path.join('detection', 'model')
-    # Define the path to the 'model.pt' file within the 'model' folder
-    model_file_path = os.path.join(model_folder, 'model.pt')
+    image_path = os.path.join(UPLOAD_FOLDER, image_files[0])
 
     output_dir = os.path.join('staticFiles', 'process_bounding_boxes')
 
-    results = run_yolo_inference(model_file_path, image_path)
+    results = run_yolo_inference(yolo_model, image_path)
 
     process_bounding_boxes(results, image_path, max_boxes_per_image=10, spacing=10,
                            output_dir=output_dir)
 
-    combined_text = perform_ocr_and_combine_text_for_sorted_images(output_dir)
+    combined_text = perform_ocr_and_combine_text_for_sorted_images(output_dir, detector)
     print(combined_text)
     return render_template("upload_image.html")
 
@@ -58,4 +62,4 @@ def upload_image():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=4000, debug=True)
--- detection/detect.py
+++ detection/detect.py
@@ -1,10 +1,15 @@
 from ultralytics import YOLO
 
-def run_yolo_inference(model_path, image_path, img_size=640, confidence=0.6, iou_threshold=0.5):
+
+def load_yolo_model(model_path):
     # Load the YOLO model
     model = YOLO(model_path)  # Load the pretrained YOLOv8n model
+    return model
+
+
+def run_yolo_inference(model, image_path, img_size=640, confidence=0.6, iou_threshold=0.5):
 
     # Run inference on the specified image
-    results = model.predict(image_path, save=True, imgsz=img_size, conf=confidence, iou=iou_threshold)
+    results = model.predict(image_path, imgsz=img_size, conf=confidence, iou=iou_threshold)
 
     return results
